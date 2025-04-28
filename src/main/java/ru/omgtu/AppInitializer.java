c5894dbf42789ebc3ddf0bbea58c4bbf9cf6b194
Faspeice <154542847+Faspeice@users.noreply.github.com>
2025-04-28 18:01:46 +0600

    Revert "refactor lr7"
    This reverts commit 00b55ba8c5f078736d67d95f8366c90ab0f20a5d.

--- src/main/java/ru/omgtu/AppInitializer.java
+++ src/main/java/ru/omgtu/AppInitializer.java
@@ -1,51 +1,32 @@
 package ru.omgtu;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
-import jakarta.servlet.annotation.WebListener;
 import ru.omgtu.controller.*;
-import ru.omgtu.factory.GunFactory;
-import ru.omgtu.repo.GunJsonRepository;
-import ru.omgtu.service.GunService;
 
-@WebListener
+
 public class AppInitializer implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        ServletContext context = sce.getServletContext();
-        ObjectMapper objectMapper = new ObjectMapper();
-        
-        GunJsonRepository repository = new GunJsonRepository(objectMapper);
-        GunService gunService = new GunService(repository);
-        
-        if (gunService.getAllGuns().isEmpty()) {
-            GunFactory.createInitialGuns().forEach(gunService::addGun);
-        }
-        
-        GunServlet gunServlet = new GunServlet(gunService, objectMapper);
-        context.addServlet("GunServlet", gunServlet).addMapping("/guns");
 
-        context
+        sce.getServletContext()
                 .addServlet("aboutServlet", new AboutServlet())
                 .addMapping("/about");
 
-        context
+        sce.getServletContext()
                 .addServlet("contactServlet", new ContactServlet())
                 .addMapping("/contact");
 
-        context
+        sce.getServletContext()
                 .addServlet("feedbackServlet", new FeedbackServlet())
                 .addMapping("/feedback");
 
-        context
+        sce.getServletContext()
                 .addServlet("homeServlet", new HomeServlet())
                 .addMapping("/home");
 
-        context
+        sce.getServletContext()
                 .addServlet("productServlet", new ProductsServlet())
                 .addMapping("/products");
     }
-
 }
